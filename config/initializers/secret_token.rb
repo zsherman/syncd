27a3c033932e64ac4fefb147d9ccbbe87d5b1509
Max Gillett <Max@Maxs-MBP.(none)>
2012-06-23 01:38:21 -0400

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syncd::Application.config.secret_token = '677d72a794ce624ed40660dd5d5fb628c9403093c055910e3ef7d9d0b66cf4864a159078f554eee62a2b206b76efbc926a754eb5454cc946dc5ede509ae2d1fb'
