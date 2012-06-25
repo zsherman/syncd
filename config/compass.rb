edf48328d25b8dfadf0737b60bcd2dfa768fb0f4
Max Gillett <max.gillett@gmail.com>
2012-06-24 21:04:34 -0400

    retina display stuff + icons

--- config/compass.rb
+++ config/compass.rb
@@ -1,2 +1,8 @@
 # Require any additional compass plugins here.
 project_type = :rails
+
+retina_ext = File.join('./lib/', 'retina')
+require File.join(retina_ext, 'lib', 'sass_extensions.rb')
+add_import_path File.join(retina_ext, 'stylesheets')
+
+
