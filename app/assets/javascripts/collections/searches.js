079dd8d6f826225f9825c38d2cd5a8e2b6af60ef
Zach Sherman <zksherm@gmail.com>
2012-08-15 00:40:48 -0400

    some pseudocode

--- app/assets/javascripts/collections/searches.js
+++ app/assets/javascripts/collections/searches.js
@@ -2,5 +2,6 @@ Syncd.Collections.Searches = Backbone.Collection.extend({
 	model: Syncd.Models.Song, 
 	url: function() {
 		return '';
+		//dynamically construct to take in search params
 	}
 });
--- app/assets/javascripts/views/searches_index.js
+++ app/assets/javascripts/views/searches_index.js
@@ -1,5 +1,7 @@
 Syncd.Views.SearchesIndex = Backbone.Marionette.CollectionView.extend({
 
+	itemView: Syncd.Views.SearchedSong
+
 	initialize: function() {
 		var _self = this;
 		_.bindAll(this);
@@ -8,8 +10,21 @@ Syncd.Views.SearchesIndex = Backbone.Marionette.CollectionView.extend({
 	        _self.render();
 	      }
 	    });
+	    //set collection
 	},
 
- 	itemView: Syncd.Views.SearchedSong
+	render: function () {
+        _.each(this.collection.models, function (song) {
+            this.renderSong(song);
+        }, this);
+    },
+
+    renderSong: function (song) {
+        var songView = new SearchedSong({
+            model: song
+        });
+        this.$el.append(songView.render().el);
+    }
+
 	
 });
