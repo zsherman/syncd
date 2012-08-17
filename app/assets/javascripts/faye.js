2bc4cc43ee9c1d2e9d2d3b83dd3321ac482e0ee6
Max Gillett <max.gillett@gmail.com>
2012-08-17 14:39:04 -0400

    dsiabled long-polling

--- app/assets/javascripts/faye.js
+++ app/assets/javascripts/faye.js
@@ -1,3 +1,4 @@
 var faye = new Faye.Client('http://glacial-oasis-6992.herokuapp.com/faye');
 faye.disable('websocket');
+faye.disable('long-polling');
 faye.subscribe("/messages/new", function(data) {alert(data)});
