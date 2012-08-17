c99e8f42f39cd5a1ffd50bdfcbc7bc90d1994cfd
Max Gillett <max.gillett@gmail.com>
2012-08-17 14:22:36 -0400

    faye server changed

--- app/assets/javascripts/faye.js
+++ app/assets/javascripts/faye.js
@@ -1,2 +1,2 @@
-var faye = new Faye.Client('http://thawing-peak-7366.herokuapp.com/faye');
+var faye = new Faye.Client('http://glacial-oasis-6992.herokuapp.com/faye');
 faye.subscribe("/messages/new", function(data) {alert(data)});
