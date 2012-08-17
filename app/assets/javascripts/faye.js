008eaa3be7f5e3dc1c9171db10a90a9c4f8087e3
Max Gillett <max.gillett@gmail.com>
2012-08-17 15:19:37 -0400

    faye

--- app/assets/javascripts/faye.js
+++ app/assets/javascripts/faye.js
@@ -1,6 +1,6 @@
-$(function() {
+
 var faye = new Faye.Client('http://glacial-oasis-6992.herokuapp.com/faye');
 faye.setHeader('Access-Control-Allow-Origin', '*');
 Faye.Transport.WebSocket.isUsable = function(_,c) { c(false) }
 faye.subscribe("/messages/new", function(data) {alert(data)});
-});
+
