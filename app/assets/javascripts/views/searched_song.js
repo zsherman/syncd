e68afc4622feaf9b4a94e42dafe1c47c730567fa
Max Gillett <max.gillett@gmail.com>
2012-08-18 00:45:06 -0400

    songs can be added to playlists through searches

--- app/assets/javascripts/views/searched_song.js
+++ app/assets/javascripts/views/searched_song.js
@@ -1,15 +1,23 @@
 Syncd.Views.SearchedSong = Backbone.Marionette.ItemView.extend({
 
   initialize: function() {
-    _.bindAll(this);
+    //_.bindAll(this);
     //this.model.initSongsonce();
   },
 
   template: "searches/searched_song",
 
+  serializeData: function(){
+    var json = this.model.toJSON();
+    json.playlists = Syncd.Instances.PlaylistCollection.toJSON();
+    console.log(json);
+    return json;
+  },
+
   events: {
   	"click.searched_song .play_search": "clickPlay",
   	"click.searched_song .stop_search": "clickStop",
+    "change .add_to_playlist": "addToPlaylist"
   },
 
   clickPlay: function() {
@@ -25,6 +33,28 @@ Syncd.Views.SearchedSong = Backbone.Marionette.ItemView.extend({
   },
 
   stop: function() {
+
+  },
+
+  addToPlaylist: function(data) {
+    var playlist_id = data.currentTarget.selectedOptions[0].value;
+    var songModel = this.model.clone();
+    var playlist = Syncd.Instances.PlaylistCollection.get(playlist_id);
+
+    var addAndSave = function() {
+      playlist.get("songs").add(songModel);
+      songModel.initSongs();
+      songModel.save();
+    }
+
+    if (playlist.get("fetched") === false) {
+      playlist.fetch({success: function(model) {
+          addAndSave();
+        }
+      });
+    } else {
+      addAndSave();
+    }
 
   }
 
